62b551902fa2c49fb76a3484b46681b39bb2ac67
justintrinh10 <justintrinh@unr.edu>
2025-10-27 12:22:09 -0700

    Refactor polar_to_cartesian for midterm

---
+++ midterm/polar_to_cartesian.py
@@ -0,0 +1,50 @@
+import math
+
+class Measurement:
+    def __init__(self, a, d):
+        self.angle = a
+        self.dist = d
+        self.x, self.y = self.polar_to_cartesian(self.angle, self.dist)
+
+    def polar_to_cartesian(self, a, d):
+        r = math.radians(a)
+        x = d * math.cos(r)
+        y = d * math.sin(r)
+        return x, y
+    
+    def output(self):
+        print(f"({self.angle:.2f}, {self.dist:.2f}) ({self.x:.2f}, {self.y:.2f})")
+    
+    def getCartesian(self):
+        return self.x, self.y
+    
+    def getPolar(self):
+        return self.angle, self.dist
+    
+    def getLidar(self):
+        return self.lidar
+
+DATA_FILE = "lidar_data.csv"
+
+def main():
+    data = read_file(DATA_FILE)
+    print(f"Data Set")
+    print("(Angle[degrees], Range[cm]) (x[cm], y[cm])")
+    data.output()
+
+def read_file(file_name):
+    measurements = [] 
+    with open(file_name, 'r') as file:
+        file.readline()
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            items = line.split(',')
+            angle = float(items[0])
+            dist = float(items[1])
+            measurements.append(Measurement(angle, dist))
+    return measurements
+
+if __name__ == "__main__":
+    main()
