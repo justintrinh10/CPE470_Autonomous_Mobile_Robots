94a71f5c8e3a9200b1be9ec7bdd30e1aef4414d5
justintrinh10 <justintrinh@unr.edu>
2025-11-19 16:19:27 -0800

    fixed camera calibraatino

--- ros2_ws/src/checkpoint_pkg/checkpoint_pkg/aruco_pose_parameters.py
+++ ros2_ws/src/checkpoint_pkg/checkpoint_pkg/aruco_pose_parameters.py
@@ -25,11 +25,11 @@ class ArucoPoseNode(Node):
 
         #Camera calibration parameters with our values
         self.camera_matrix = np.array([
-            [4238.949564, 0, 357.825050],
-            [0, 4278.343793, 214.944885],
+            [821.993, 0, 330.489],
+            [0, 821.993, 248.997],
             [0, 0, 1]
         ])
-        self.dist_coeffs = np.array([[-0.018522, 1.03979, 0, 0, -3.3171,]], dtype=np.float32)
+        self.dist_coeffs = np.array([[-0.018522, 1.03979, 0, 0, -3.3171, 0, 0, 0]])
         self.marker_length = 0.05  # meters
 
         # Timer to process frames (~10 FPS)
