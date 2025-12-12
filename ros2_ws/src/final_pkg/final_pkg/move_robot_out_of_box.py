900f6ad3bfc0d2ed28b07b2c55d2c53a024aeedd
justintrinh10 <justintrinh@unr.edu>
2025-12-11 22:29:47 -0800

    Moving robot out of box node implementation

--- ros2_ws/src/final_pkg/final_pkg/move_robot_out_of_box.py
+++ ros2_ws/src/final_pkg/final_pkg/move_robot_out_of_box.py
@@ -3,7 +3,10 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from std_msgs.msg import Bool
+from std_msgs.msg import Float32
 import numpy as np
+from point import Point
+from path import Path
 
 class MoveRobotOutOfBox(Node):
     def __init__(self):
@@ -27,14 +30,63 @@ class MoveRobotOutOfBox(Node):
         )
 
         self.subscriber_move_robot_out_of_box_ = self.create_subscription(
-            Path_Msg,
+            String,
             "move_robot_out_of_box",
             self.listener_callback_move_robot_out_of_box,
             10,
         )
 
+        self.moving = False
+        self.rotating = False
 
+    def listener_callback_move_robot_out_of_box(self, msg):
+        path = self.convert_msg_to_path(msg)
+        for i in range(len(path.points)):
+            angle_command = path.angle_changes[i]
+            distance_command = path.distances[i]
+            angle_msg = Float32()
+            angle_msg.data = float(angle_command)
+            self.publisher_rotate_angle_.publish(angle_msg)
+            self.rotating = True
 
+            while self.rotating:
+                rclpy.spin_once(self)
 
+            distance_msg = Float32()
+            distance_msg.data = float(distance_command)
+            self.publisher_move_distance_.publish(distance_msg)
+            self.moving = True
 
+            while self.moving:
+                rclpy.spin_once(self)
 
+        complete_msg = Bool()
+        complete_msg.data = True
+        self.publisher_.publish(complete_msg)
+            
+    def listener_callback_move_distance_complete(self, msg):
+        self.moving = False
+
+    def listener_callback_rotate_angle_complete(self, msg):
+        self.rotating = False
+
+    def convert_msg_to_path(self, msg):
+        path_points = msg.data.strip().split("\n")
+        points = []
+        for point_str in path_points:
+            x_str, y_str = point_str.split(",")
+            x = float(x_str)
+            y = float(y_str)
+            points.append(Point(x, y))
+        path = Path(points)
+        return path
+
+def main(args=None):
+    rclpy.init(args=args)
+    move_robot_out_of_box = MoveRobotOutOfBox()
+    rclpy.spin(move_robot_out_of_box)
+    move_robot_out_of_box.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
