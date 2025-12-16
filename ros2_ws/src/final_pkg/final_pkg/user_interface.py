3c62f494b33ab2f541e464fefeb7b2e6834874f8
justintrinh10 <justintrinh@unr.edu>
2025-12-16 15:41:55 -0800

    redid user_interface

--- ros2_ws/src/final_pkg/final_pkg/user_interface.py
+++ ros2_ws/src/final_pkg/final_pkg/user_interface.py
@@ -1,30 +1,22 @@
-import serial
-import math
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from std_msgs.msg import Bool
-import numpy as np
+from std_msgs.msg import String, Bool
+import threading
 
 class UserInterface(Node):
     def __init__(self):
         super().__init__("user_interface")
-
         self.publisher_ = self.create_publisher(String, "user_interface_complete", 10)
+        self.create_subscription(Bool, "start_user_interface", self.listener_callback, 10)
+        self.get_logger().info("User Interface Node has been started.")
 
-        self.subscriber_ = self.create_subscription(
-            Bool,
-            "start_user_interface",
-            self.listener_callback,
-            10,
-        )
-    
     def listener_callback(self, msg):
-        self.get_logger().info("Enter desired point. (meters)")
-        self.get_logger().info("X: ")
-        x = float(input())
-        self.get_logger().info("Y: ")
-        y = float(input())
+        threading.Thread(target=self.get_user_input).start()
+
+    def get_user_input(self):
+        self.get_logger().info("Enter desired point (meters)")
+        x = float(input("X: "))
+        y = float(input("Y: "))
         point_msg = String()
         point_msg.data = f"{x},{y}"
         self.publisher_.publish(point_msg)
@@ -32,9 +24,9 @@ class UserInterface(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    user_interface = UserInterface()
-    rclpy.spin(user_interface)
-    user_interface.destroy_node()
+    node = UserInterface()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == "__main__":
